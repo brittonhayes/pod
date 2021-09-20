8651884319c3e7908e1e72d8b0e7159ef9d849d3
Britton Hayes <brittonhayes@users.noreply.github.com>
2021-09-19 22:08:50 -0700

    feat: new bind package for ui methods

---
+++ backend/bind/bind.go
@@ -0,0 +1,99 @@
+package bind
+
+import (
+	"strings"
+
+	"github.com/brittonhayes/pod/backend/client"
+	"github.com/brittonhayes/pod/backend/project"
+	"github.com/brittonhayes/pod/backend/store"
+	"github.com/mitchellh/mapstructure"
+	"github.com/wailsapp/wails"
+)
+
+type Storage struct {
+	r      *wails.Runtime
+	logger *wails.CustomLogger
+}
+
+func (s *Storage) WailsInit(runtime *wails.Runtime) error {
+	s.r = runtime
+	s.logger = s.r.Log.New("Storage")
+
+	return nil
+}
+
+func (s *Storage) SaveClient(payload map[string]interface{}) (*client.Client, error) {
+	c := client.NewClient()
+	err := mapstructure.Decode(payload, &c)
+	if err != nil {
+		return c, err
+	}
+
+	_, err = c.Save()
+	if err != nil {
+		s.logger.Error(err.Error())
+		return c, err
+	}
+
+	return c, nil
+}
+
+func (s *Storage) QueryClients(field, value string) ([]client.Client, error) {
+	c := client.NewClient()
+	results := []client.Client{}
+	_, err := c.Query(strings.ToTitle(field), value, results)
+	if err != nil {
+		s.logger.Error(err.Error())
+		return []client.Client{}, err
+	}
+
+	return results, nil
+}
+
+func (s *Storage) ListProjects() ([]project.Project, error) {
+
+	results := []project.Project{}
+	db, err := store.NewDB("project")
+	if err != nil {
+		s.logger.Error(err.Error())
+		return nil, err
+	}
+	defer db.Close()
+
+	err = db.List(&results)
+	if err != nil {
+		s.logger.Error(err.Error())
+		return nil, err
+	}
+
+	return results, err
+}
+
+func (s *Storage) QueryProjects(field, value string) ([]project.Project, error) {
+	p := project.NewProject()
+	results := []project.Project{}
+	_, err := p.Query(strings.ToTitle(field), value, results)
+	if err != nil {
+		s.logger.Error(err.Error())
+		return []project.Project{}, err
+	}
+
+	return results, nil
+}
+
+func (s *Storage) SaveProject(payload map[string]interface{}) (*project.Project, error) {
+	p := project.NewProject()
+	err := mapstructure.Decode(payload, &p)
+	if err != nil {
+		s.logger.Error(err.Error())
+		return p, err
+	}
+
+	_, err = p.Save()
+	if err != nil {
+		s.logger.Error(err.Error())
+		return p, err
+	}
+
+	return p, nil
+}
---
+++ backend/bind/project.go
@@ -0,0 +1,52 @@
+package bind
+
+import (
+	"fmt"
+
+	"github.com/brittonhayes/pod/backend/project"
+	"github.com/mitchellh/mapstructure"
+	"github.com/wailsapp/wails"
+)
+
+type ProjectState struct {
+	current *project.Project
+
+	r      *wails.Runtime
+	logger *wails.CustomLogger
+	store  *wails.Store
+}
+
+// WailsInit is called when the component is being initialised
+func (s *ProjectState) WailsInit(runtime *wails.Runtime) error {
+	s.current = project.NewProject()
+	s.r = runtime
+	s.logger = runtime.Log.New("Project State")
+	s.store = runtime.Store.New("project", project.NewProject())
+
+	s.store.Subscribe(func(state *project.Project) {
+		s.logger.Info(fmt.Sprintf("%#v", state))
+	})
+
+	return nil
+}
+
+func (s *ProjectState) Set(payload map[string]interface{}) {
+	p := project.NewProject()
+	err := mapstructure.Decode(p, &payload)
+	if err != nil {
+		s.logger.Error(err.Error())
+		return
+	}
+
+	s.logger.Info(fmt.Sprintf("Setting value: %s", p.Name))
+	s.current = p
+	s.store.Set(p)
+}
+
+func (s *ProjectState) Value() *project.Project {
+	s.store.Update(func(current *project.Project) *project.Project {
+		return s.current
+	})
+
+	return s.current
+}
