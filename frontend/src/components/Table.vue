8efaaa090667640c8c18e4d9743f932e9ade9929
Britton Hayes <brittonhayes@users.noreply.github.com>
2021-09-21 01:57:07 -0700

    fix: converted project table to re-usable table

---
+++ frontend/src/components/Table.vue
@@ -0,0 +1,30 @@
+<template>
+  <section>
+    <b-table :data="data" :columns="columns" :default-sort="sort" paginated>
+      <template #empty>
+        <div class="notification has-text-centered">No results</div>
+      </template>
+    </b-table>
+  </section>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  props: {
+    data: {
+      type: Array,
+      required: true,
+    },
+    columns: {
+      type: Array,
+      required: true,
+    },
+    sort: {
+      type: Array,
+      required: true,
+    },
+  },
+});
+</script>
