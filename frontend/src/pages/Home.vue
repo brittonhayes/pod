62c03966576ec08db2c84d4dc0e0bd080f5ed2bb
Britton Hayes <brittonhayes@users.noreply.github.com>
2021-09-21 02:12:23 -0700

    fix: removed unused code from homepage

--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -1,19 +1,6 @@
 <template>
   <container>
     <Banner :title="title" :subtitle="subtitle" />
-    <section class="section">
-      <div class="columns is-multiline">
-        <card color="success" class="p-0">
-          <h1 class="is-size-2 p-1 has-text-centered">🌴</h1>
-        </card>
-        <card color="success" class="p-0">
-          <h1 class="is-size-2 p-1 has-text-centered">🌴</h1>
-        </card>
-        <card color="success" class="p-0">
-          <h1 class="is-size-2 p-1 has-text-centered">🌴</h1>
-        </card>
-      </div>
-    </section>
   </container>
 </template>
 
@@ -22,7 +9,6 @@ import Vue from "vue";
 import Page from "@/mixins/Page";
 import Container from "@/components/Container.vue";
 import Banner from "@/components/Banner.vue";
-import Card from "@/components/Card.vue";
 
 export default Vue.extend({
   name: "Home",
@@ -30,12 +16,6 @@ export default Vue.extend({
   components: {
     Container,
     Banner,
-    Card,
-  },
-  data() {
-    return {
-      media: 3,
-    };
   },
 });
 </script>
