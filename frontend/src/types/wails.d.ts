bf0ad8b29f27fb544c94f665681555532146d4db
Britton Hayes <brittonhayes@users.noreply.github.com>
2021-09-24 15:25:44 -0700

    fix: rebuilt application

--- frontend/src/types/wails.d.ts
+++ frontend/src/types/wails.d.ts
@@ -1,28 +1,30 @@
 interface Config {
-  WailsInit(...args: any[]): Promise<any>;
+	WailsInit(...args : any[]):Promise<any>
 }
 interface Storage {
-  ListProjects(...args: any[]): Promise<any>;
-  QueryClients(...args: any[]): Promise<any>;
-  QueryProjects(...args: any[]): Promise<any>;
-  SaveClient(...args: any[]): Promise<any>;
-  SaveProject(...args: any[]): Promise<any>;
+	ListClients(...args : any[]):Promise<any>
+	ListProjects(...args : any[]):Promise<any>
+	QueryClients(...args : any[]):Promise<any>
+	QueryProjects(...args : any[]):Promise<any>
+	SaveClient(...args : any[]):Promise<any>
+	SaveProject(...args : any[]):Promise<any>
+	WailsInit(...args : any[]):Promise<any>
 }
 interface ProjectState {
-  Set(...args: any[]): Promise<any>;
-  Update(...args: any[]): Promise<any>;
-  WailsInit(...args: any[]): Promise<any>;
+	Set(...args : any[]):Promise<any>
+	Value(...args : any[]):Promise<any>
+	WailsInit(...args : any[]):Promise<any>
 }
 
 interface Backend {
-  Config: Config;
-  Storage: Storage;
-  ProjectState: ProjectState;
+	Config: Config
+	Storage: Storage
+	ProjectState: ProjectState
 }
 
 declare global {
-  interface Window {
-    backend: Backend;
-  }
+	interface Window {
+		backend: Backend;
+	}
 }
 export {};
